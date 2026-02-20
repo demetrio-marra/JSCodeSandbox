483f5a83dc862f7b2cd551fb151e323aa7765651
Demetrio Marra <demetrio.marra3@gmail.com>
2026-02-20 11:30:18 +0100

    Rename provisionedEnvironmentId to provisionedEnvironmentName
    Updated the ExecuteCodeAsync method in ICodeExecutionService to use provisionedEnvironmentName instead of provisionedEnvironmentId. Adjusted XML documentation to match the new parameter name and clarify its purpose.

--- JSCodeSandbox/Services/ICodeExecutionService.cs
+++ JSCodeSandbox/Services/ICodeExecutionService.cs
@@ -6,11 +6,11 @@ namespace JSCodeSandbox.Services
     {
         /// <summary>
         /// Runs the provided code in a sandboxed environment and returns the output.
-        /// The provisionedEnvironmentId is used to identify which sandbox environment to use for execution, allowing for isolation and resource management.
+        /// The provisionedEnvironmentName is used to identify which sandbox environment to use for execution, allowing for isolation and resource management.
         /// </summary>
         /// <param name="input">The input containing the code to execute and the user ID.</param>
-        /// <param name="provisionedEnvironmentId">The ID of the provisioned environment to run the code in.</param>
+        /// <param name="provisionedEnvironmentName">The name of the provisioned environment to run the code in.</param>
         /// <returns>The output of the code execution.</returns>
-        Task<CodeExecutionResult> ExecuteCodeAsync(CodeExecutionRequest input, string provisionedEnvironmentId);
+        Task<CodeExecutionResult> ExecuteCodeAsync(CodeExecutionRequest input, string provisionedEnvironmentName);
     }
 }
