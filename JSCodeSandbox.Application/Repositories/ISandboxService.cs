1d0560949f5d6a318863c8beb90aa9b8def5b174
Demetrio Marra <demetrio.marra3@gmail.com>
2026-02-20 16:47:27 +0100

    Add ISandboxService interface for code execution
    Introduced the JSCodeSandbox.Application.Repositories namespace and defined the ISandboxService interface. The interface provides an async RunCode method for executing code in a sandboxed environment.

---
+++ JSCodeSandbox.Application/Repositories/ISandboxService.cs
@@ -0,0 +1,7 @@
+﻿namespace JSCodeSandbox.Application.Repositories
+{
+    public interface ISandboxService
+    {
+        Task<string> RunCode(string environmentBasePath, string userAgentId, string code);
+    }
+}
